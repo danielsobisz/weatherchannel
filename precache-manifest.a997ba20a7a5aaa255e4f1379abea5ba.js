e317ffd4db2e955af5abf07f8a4ee6b3d22ad4f8
Daniel Sobisz <dsobisz97@gmail.com>
2020-03-29 18:14:07 +0200

    Updates

---
+++ precache-manifest.a997ba20a7a5aaa255e4f1379abea5ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7fdbc76caccbcab85573c99a84fbec",
+    "url": "/weatherchannel/./index.html"
+  },
+  {
+    "revision": "41c8765c2e22dea30b8f",
+    "url": "/weatherchannel/./static/css/main.5a96b8a8.chunk.css"
+  },
+  {
+    "revision": "af7029dfadeca020fa2a",
+    "url": "/weatherchannel/./static/js/2.2522ccee.chunk.js"
+  },
+  {
+    "revision": "c6f2a8cff5bda75ce9d9d2e5dad3a2a7",
+    "url": "/weatherchannel/./static/js/2.2522ccee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41c8765c2e22dea30b8f",
+    "url": "/weatherchannel/./static/js/main.4117e124.chunk.js"
+  },
+  {
+    "revision": "5efc3a0997d76b01c2b7",
+    "url": "/weatherchannel/./static/js/runtime-main.e3d18a8b.js"
+  },
+  {
+    "revision": "051b653507a705f390c2124db0c684b3",
+    "url": "/weatherchannel/./static/media/cloud.051b6535.png"
+  }
+]);
